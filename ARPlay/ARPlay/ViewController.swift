f7b1a376ec4178980dcf1e1975a0578112d2e537
Eric Berna <eric@cidesigninc.com>
2017-07-22 11:59:27 -0500

    Abandon cupps scene.

--- ARPlay/ARPlay/ViewController.swift
+++ ARPlay/ARPlay/ViewController.swift
@@ -24,7 +24,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = CupsScene()
+        let scene = SCNScene(named: "art.scnassets/RedPlasticCup.scn")!
+        
         // Set the scene to the view
         sceneView.scene = scene
     }
